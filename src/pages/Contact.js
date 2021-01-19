5b79ef19829f893ce6467725825df67672b2c5b2
Chris Dominy <cdominy@utexas.edu>
2021-01-18 18:20:25 -0600

    Added phone number

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,10 @@
 import React from "react"
 import { Container, Col, Row } from "../components/Grid";
-import {H1} from "../components/Headers"
+import {H1, H2} from "../components/Headers"
 import Form from "../components/Form"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+
 
 function Contact(){
     return (
@@ -9,6 +12,7 @@ function Contact(){
         <Row>
             <Col size="col-md-12">
               <H1 classType="text-center contact">Contact Me</H1>
+              <H2 classType="text-center"><FontAwesomeIcon icon={faPhone}> </FontAwesomeIcon>: 513-348-8164</H2>
         <Form></Form>
     </Col>
   </Row>
