01eaa6e1740efc6eb18f493fcbac78648025fdf7
Chris Dominy <cdominy@utexas.edu>
2021-01-21 11:57:12 -0600

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,8 +2,6 @@ import React from "react"
 import { Container, Col, Row } from "../components/Grid";
 import {H1, H2} from "../components/Headers"
 import Form from "../components/Form"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPhone } from '@fortawesome/free-solid-svg-icons'
 
 
 function Contact(){
@@ -12,7 +10,6 @@ function Contact(){
         <Row>
             <Col size="col-md-12">
               <H1 classType="text-center contact">Contact Me</H1>
-              <H2 classType="text-center"><FontAwesomeIcon icon={faPhone}> </FontAwesomeIcon>: 513-348-8164</H2>
         <Form></Form>
     </Col>
   </Row>
